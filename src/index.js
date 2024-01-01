7a03d268db08e943ca0eb234063cfb357789b208
vivekrajput-93 <viveksinghraajputt@gmail.com>
2024-01-01 19:02:04 +0530

    add role model

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const apiRoutes = require("./routes/index")
 
 const app = express();
 
+const db = require("./models/index")
+
+const { User, Role } = require("./models/index")
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
@@ -14,8 +19,17 @@ app.use('/api', apiRoutes)
 const StartServer = () => {
 
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Server Started on port ${PORT}`)
+    if(process.env.DB_SYNC) {
+        db.sequelize.sync({alter : true})
+
+      }
+
+
+      const u1 = await User.findByPk(1);
+      const r1 = await Role.findByPk(1);
+      u1.addRole(r1);
   })
 }
 
